8e53529573b77279cbd186075638dcb50b703b6f
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2022-11-04 10:49:27 +0100

    summ of digestates

--- lit_data/script/summ.R
+++ lit_data/script/summ.R
@@ -9,6 +9,3 @@ dl <- dat[! is.na(dat$co.dig), ]
 summ <- aggregate2(dl, x = c('DM', 'pH', 'TAN', 'totN'), by = c('slurry.major'), 
                    FUN = list(mean = mean, sd = sd, n = length), na.rm = TRUE)
 summ <- rounddf(summ, 3, fun = signif)
-
-#write.xlsx(summ, file = '../output/lit_summ.xlsx')
-
