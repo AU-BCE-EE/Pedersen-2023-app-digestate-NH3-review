1cac079719cf27fa8b547d0268d3ded8ba22c23c
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-02-23 07:03:11 -0500

    update summaries

--- lit_raw_vs_dig/scripts/export.R
+++ lit_raw_vs_dig/scripts/export.R
@@ -1,6 +1,6 @@
 
 #write.xlsx(summ, file = '../output/lit_summ.xlsx')
 write.csv(summ, '../output/lit_summ.csv', row.names = FALSE)
-write.csv(counts, '../output/lit_counts.csv', row.names = FALSE)
+write.csv(counts, '../output/lit_counts.csv', row.names = TRUE)
 write.csv(qnt.dig, '../output/dig_quantiles.csv', row.names = FALSE)
 
