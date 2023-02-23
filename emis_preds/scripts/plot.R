e90105e69becaa512dd209c9bd0825d852afed8b
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-02-23 19:51:30 +0100

    making plot for presentation

--- emis_preds/scripts/plot.R
+++ emis_preds/scripts/plot.R
@@ -58,3 +58,66 @@ ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
   ylim(0, NA) +
   theme(legend.position = 'top')
 ggsave('../plots/emis_pred2.png', height = 4, width = 7)
+
+
+# Plot JP presentation 230224
+
+dd$man.source <- gsub('^Pig', 'Pig DK EF', dd$man.source)
+dd$man.source <- gsub('^Cattle', 'Cattle DK EF', dd$man.source)
+dd$man.source <- gsub('^Digestate A', 'Digestate DK EF', dd$man.source)
+dd$man.source <- gsub('^Digestate B', 'Digestate MAG', dd$man.source)
+dd$man.source <- gsub('^Digestate C', 'Digestate lit', dd$man.source)
+
+ul$man.source <- gsub('^Digestate B', 'Digestate MAG', ul$man.source)
+ulc$man.source <- gsub('^Digestate C', 'Digestate lit', ulc$man.source)
+
+ddEF <- subset(dd, man.source != 'Digestate lit' & man.source != 'Digestate MAG')
+
+ggplot(ddEF, aes(app.timing.int, EFp, colour = man.source)) +
+  geom_line(lty = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~ raw.or.dig) +
+  theme_bw() + 
+  labs(x = 'Application period', y = 'Emission (% of TAN)',
+       shape = '', colour = '') + 
+  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
+                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+  ylim(0, 62) +
+  theme(legend.position = 'top') + 
+  scale_color_brewer(palette = 'Dark2')
+ggsave('../plots/emis_pred2_JP1.png', height = 4, width = 7)
+
+ggplot(dd[! dd$man.source == 'Digestate MAG', ], aes(app.timing.int, EFp, colour = man.source)) +
+#  geom_line(data = ul, alpha = 0.7) +
+  geom_line(data = ulc, alpha = 0.7) +
+  geom_line(lty = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~ raw.or.dig) +
+  theme_bw() + 
+  labs(x = 'Application period', y = 'Emission (% of TAN)',
+       shape = '', colour = '') + 
+  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
+                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+  ylim(0, 62) +
+  theme(legend.position = 'top') + 
+  scale_color_brewer(palette = 'Dark2')
+ggsave('../plots/emis_pred2_JP2.png', height = 4, width = 7)
+
+ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
+  geom_line(data = ul, alpha = 0.7) +
+  geom_line(data = ulc, alpha = 0.7) +
+  geom_line(lty = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~ raw.or.dig) +
+  theme_bw() + 
+  labs(x = 'Application period', y = 'Emission (% of TAN)',
+       shape = '', colour = '') + 
+  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
+                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+  ylim(0, 62) +
+  theme(legend.position = 'top') + 
+  scale_color_brewer(palette = 'Dark2')
+ ggsave('../plots/emis_pred2_JP3.png', height = 4, width = 7)
