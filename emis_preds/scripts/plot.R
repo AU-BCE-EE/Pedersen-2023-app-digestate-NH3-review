803620b34fa1ba1219f2592f9503027de54b84b9
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-02-24 11:38:39 -0500

    Tweak plot for paper

--- emis_preds/scripts/plot.R
+++ emis_preds/scripts/plot.R
@@ -14,7 +14,7 @@ ul2 <- ul
 ul2$app.timing.int <- as.integer(ul2$app.timing) + 0.5
 ul2$EFp <- NA
 ul <- rbind(ul, ul2)
-ul$app.timing.int <- ul$app.timing.int + 0.15
+ul$app.timing.int <- ul$app.timing.int + 0.11
 
 # Repeat all this for second set of error bars on Dig. C
 ulc <- dat[grepl('C high|C low', dat$man.source), ]
@@ -23,30 +23,30 @@ ulc2 <- ulc
 ulc2$app.timing.int <- as.integer(ulc2$app.timing) + 0.5
 ulc2$EFp <- NA
 ulc <- rbind(ulc, ulc2)
-ulc$app.timing.int <- ulc$app.timing.int - 0.15
+ulc$app.timing.int <- ulc$app.timing.int - 0.11
 
 # Shift x position to avoid overplotting
-dd$app.timing.int[dd$man.source == 'Digestate B'] <- dd$app.timing.int[dd$man.source == 'Digestate B'] + 0.15
-dd$app.timing.int[dd$man.source == 'Digestate C'] <- dd$app.timing.int[dd$man.source == 'Digestate C'] - 0.15
+dd$app.timing.int[dd$man.source == 'Digestate B'] <- dd$app.timing.int[dd$man.source == 'Digestate B'] + 0.11
+dd$app.timing.int[dd$man.source == 'Digestate C'] <- dd$app.timing.int[dd$man.source == 'Digestate C'] - 0.11
 
 labpos <- subset(dd, app.timing == 'Autumn')
 
 ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
-  geom_label(data = labpos, aes(label = dig.type), nudge_x = 0.2, hjust = 'left', 
-             show.legend = FALSE) +
+  #geom_label(data = labpos, aes(label = dig.type), nudge_x = 0.2, hjust = 'left', show.legend = FALSE) +
+  geom_point(size = 2.3) +
   geom_line(data = ul, alpha = 0.7) +
   geom_line(data = ulc, alpha = 0.7) +
-  geom_line(lty = 1) +
-  geom_point(size = 3) +
+  geom_line() +
   facet_wrap(~ raw.or.dig) +
   labs(x = 'Application period', y = 'Emission (% of TAN)',
        shape = '', colour = '') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
-                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+                     labels= unique(dat$app.timing), limits = c(0.8, 5.2)) +
   ylim(0, NA) +
+  theme_bw() +
   theme(legend.position = 'top')
-ggsave('../plots/emis_pred2.png', height = 4, width = 7)
+ggsave('../plots/emis_pred.png', height = 3, width = 6)
 
 
 # Plot JP presentation 230224
