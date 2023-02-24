563af4cbc10c53fec8d3ab077160d6e5f2b86e16
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-02-24 11:28:29 -0500

    Clean up plot code a bit

--- emis_preds/scripts/plot.R
+++ emis_preds/scripts/plot.R
@@ -1,34 +1,22 @@
-
-
-labpos <- subset(dat, app.timing == 'Autumn')
-
-ggplot(dat, aes(as.integer(app.timing), EFp, shape = man.descrip, colour = man.descrip)) +
-  geom_label(data = labpos, aes(label = man.descrip), nudge_x = 0.1, hjust = 'left', 
-             show.legend = FALSE) +
-  geom_point(size = 3) +
-  geom_line(lty = 1) +
-  labs(x = 'Application period', y = 'Emission (% of TAN)',
-       shape = '', colour = '') + 
-  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
-  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
-                     labels= unique(dat$app.timing), limits = c(1, 6.1)) +
-  ylim(0, NA) +
-  theme(legend.position = 'top')
-ggsave('../plots/emis_pred.png', height = 4, width = 6)
+# Plots
 
 # Sort out error bars and labels
+# This is a bit crude, partially to avoid having to reshape data and use geom_errorbar, partially for fine control over plotting
+# Need numeric x to plot lines in my experience
 dat$app.timing.int <- as.integer(dat$app.timing)
 dd <- dat[!grepl('high|low', dat$man.source), ]
 
-
+# ul = "upper/lower" for error bars
 ul <- dat[grepl('B high|B low', dat$man.source), ]
 ul$man.source <- 'Digestate B'
 ul2 <- ul
+# Add some intermediate rows with y = NA to break lines
 ul2$app.timing.int <- as.integer(ul2$app.timing) + 0.5
 ul2$EFp <- NA
 ul <- rbind(ul, ul2)
 ul$app.timing.int <- ul$app.timing.int + 0.15
 
+# Repeat all this for second set of error bars on Dig. C
 ulc <- dat[grepl('C high|C low', dat$man.source), ]
 ulc$man.source <- 'Digestate C'
 ulc2 <- ulc
@@ -37,6 +25,7 @@ ulc2$EFp <- NA
 ulc <- rbind(ulc, ulc2)
 ulc$app.timing.int <- ulc$app.timing.int - 0.15
 
+# Shift x position to avoid overplotting
 dd$app.timing.int[dd$man.source == 'Digestate B'] <- dd$app.timing.int[dd$man.source == 'Digestate B'] + 0.15
 dd$app.timing.int[dd$man.source == 'Digestate C'] <- dd$app.timing.int[dd$man.source == 'Digestate C'] - 0.15
 
