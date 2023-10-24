86791186417848435e978ea8c17be0f3bfca46c7
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-10-24 15:02:57 +0200

    making plots for presentation

--- emis_preds/scripts/plot.R
+++ emis_preds/scripts/plot.R
@@ -3,7 +3,7 @@
 # Sort out error bars and labels
 # This is a bit crude, partially to avoid having to reshape data and use geom_errorbar, partially for fine control over plotting
 # Need numeric x to plot lines in my experience
-dat$app.timing.int <- as.integer(dat$app.timing)
+dat$app.timing.int <- as.integer(as.factor(dat$app.timing))
 dd <- dat[!grepl('high|low', dat$man.source), ]
 
 # ul = "upper/lower" for error bars
@@ -11,7 +11,7 @@ ul <- dat[grepl('B high|B low', dat$man.source), ]
 ul$man.source <- 'Digestate B'
 ul2 <- ul
 # Add some intermediate rows with y = NA to break lines
-ul2$app.timing.int <- as.integer(ul2$app.timing) + 0.5
+ul2$app.timing.int <- as.integer(as.factor(ul2$app.timing)) + 0.5
 ul2$EFp <- NA
 ul <- rbind(ul, ul2)
 ul$app.timing.int <- ul$app.timing.int + 0.11
@@ -20,7 +20,7 @@ ul$app.timing.int <- ul$app.timing.int + 0.11
 ulc <- dat[grepl('C high|C low', dat$man.source), ]
 ulc$man.source <- 'Digestate C'
 ulc2 <- ulc
-ulc2$app.timing.int <- as.integer(ulc2$app.timing) + 0.5
+ulc2$app.timing.int <- as.integer(as.factor(ulc2$app.timing)) + 0.5
 ulc2$EFp <- NA
 ulc <- rbind(ulc, ulc2)
 ulc$app.timing.int <- ulc$app.timing.int - 0.11
@@ -41,7 +41,7 @@ ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
   labs(x = 'Application period', y = 'EF (% of TAN)',
        shape = '', colour = '') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
-  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
+  scale_x_continuous(breaks = unique(as.integer(as.factor(dat$app.timing))), 
                      labels= unique(dat$app.timing), limits = c(0.8, 5.2)) +
   ylim(0, NA) +
   theme_bw() +
@@ -61,70 +61,85 @@ ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
        shape = '', colour = '') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
-                     labels= unique(dat$app.timing), limits = c(0.8, 5.2)) +
+                     labels= unique(as.integer(dat$app.timing)), limits = c(0.8, 5.2)) +
   ylim(0, NA) +
   theme_bw() +
   theme(legend.position = 'top')
 ggsave2x('../plots/emis_pred_Ramiran', height = 3.5, width = 5.8)
 
 
-# # Plot JP presentation 230224
-# dd$man.source <- gsub('^Pig', 'Pig DK EF', dd$man.source)
-# dd$man.source <- gsub('^Cattle', 'Cattle DK EF', dd$man.source)
-# dd$man.source <- gsub('^Digestate A', 'Digestate DK EF', dd$man.source)
-# dd$man.source <- gsub('^Digestate B', 'Digestate MAG', dd$man.source)
-# dd$man.source <- gsub('^Digestate C', 'Digestate lit', dd$man.source)
-# 
-# ul$man.source <- gsub('^Digestate B', 'Digestate MAG', ul$man.source)
-# ulc$man.source <- gsub('^Digestate C', 'Digestate lit', ulc$man.source)
-# 
-# ddEF <- subset(dd, man.source != 'Digestate lit' & man.source != 'Digestate MAG')
-# 
-# ggplot(ddEF, aes(app.timing.int, EFp, colour = man.source)) +
-#   geom_line(lty = 1) +
-#   geom_point(size = 3) +
-#   facet_wrap(~ raw.or.dig) +
-#   theme_bw() + 
-#   labs(x = 'Application period', y = 'Emission (% of TAN)',
-#        shape = '', colour = '') + 
-#   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
-#   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
-#                      labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
-#   ylim(0, 62) +
-#   theme(legend.position = 'top') + 
-#   scale_color_manual(values = c("#1B9E77", "#D95F02", "#7570B3"))
-# ggsave('../plots/emis_pred2_JP1.png', height = 4, width = 7)
-# 
-# ggplot(dd[! dd$man.source == 'Digestate MAG', ], aes(app.timing.int, EFp, colour = man.source)) +
-# #  geom_line(data = ul, alpha = 0.7) +
-#   geom_line(data = ulc, alpha = 0.7) +
-#   geom_line(lty = 1) +
-#   geom_point(size = 3) +
-#   facet_wrap(~ raw.or.dig) +
-#   theme_bw() + 
-#   labs(x = 'Application period', y = 'Emission (% of TAN)',
-#        shape = '', colour = '') + 
-#   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
-#   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
-#                      labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
-#   ylim(0, 62) +
-#   theme(legend.position = 'top') + 
-#   scale_color_manual(values = c("#1B9E77",  "#D95F02","#E7298A", "#7570B3"))
-# ggsave('../plots/emis_pred2_JP2.png', height = 4, width = 7)
-# 
-# ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
-#   geom_line(data = ul, alpha = 0.7) +
-#   geom_line(data = ulc, alpha = 0.7) +
-#   geom_line(lty = 1) +
-#   geom_point(size = 3) +
-#   facet_wrap(~ raw.or.dig) +
-#   theme_bw() + 
-#   labs(x = 'Application period', y = 'Emission (% of TAN)',
-#        shape = '', colour = '') + 
-#   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
-#   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), 
-#                      labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
-#   ylim(0, 62) +
-#   theme(legend.position = 'top') + 
-#   scale_color_manual(values = c("#1B9E77", "#D95F02","#E7298A", "#66A61E", "#7570B3"))
-#  ggsave('../plots/emis_pred2_JP3.png', height = 4, width = 7)
+# # Plot JP presentation 231031
+dd$man.source <- gsub('^Pig', 'Svinegylle', dd$man.source)
+dd$man.source <- gsub('^Cattle', 'Kvæggylle', dd$man.source)
+dd$man.source <- gsub('^Digestate A', 'Afgasset A', dd$man.source)
+dd$man.source <- gsub('^Digestate B', 'Afgasset C', dd$man.source)
+dd$man.source <- gsub('^Digestate C', 'Afgasset B', dd$man.source)
+
+ulc$man.source <- gsub('^Digestate B', 'Afgasset C', ulc$man.source)
+ulc$man.source <- gsub('^Digestate C', 'Afgasset B', ulc$man.source)
+
+ul$man.source <- gsub('^Digestate B', 'Afgasset C', ul$man.source)
+ul$man.source <- gsub('^Digestate C', 'Afgasset B', ul$man.source)
+
+dd$man.source <- gsub('^Pig', 'Svinegylle', dd$man.source)
+dd$man.source <- gsub('^Cattle', 'Kvæggylle', dd$man.source)
+
+dd$raw.or.dig <- gsub('^Raw', 'Ubehandlet', dd$raw.or.dig)
+dd$raw.or.dig <- gsub('^Digestate', 'Afgasset', dd$raw.or.dig)
+
+ulc$raw.or.dig <- gsub('^Raw', 'Ubehandlet', ulc$raw.or.dig)
+ulc$raw.or.dig <- gsub('^Digestate', 'Afgasset', ulc$raw.or.dig)
+
+ul$raw.or.dig <- gsub('^Raw', 'Ubehandlet', ul$raw.or.dig)
+ul$raw.or.dig <- gsub('^Digestate', 'Afgasset', ul$raw.or.dig)
+
+ddEF <- subset(dd, man.source != 'Afgasset C' & man.source != 'Afgasset B')
+
+ggplot(ddEF, aes(app.timing.int, EFp, colour = man.source)) +
+  geom_line(lty = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~ raw.or.dig) +
+  theme_bw() +
+  labs(x = 'Application period', y = 'Emission (% of TAN)',
+       shape = '', colour = '') +
+  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)),
+                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+  ylim(0, 62) +
+  theme(legend.position = 'top') +
+  scale_color_manual(values = c("#1B9E77", "#D95F02", "#7570B3"))
+ggsave('../plots/emis_pred2_JP1.png', height = 4, width = 7)
+
+ggplot(dd[! dd$man.source == 'Afgasset C', ], aes(app.timing.int, EFp, colour = man.source)) +
+#  geom_line(data = ul, alpha = 0.7) +
+  geom_line(data = ulc[! ulc$man.source == 'Afgasset C', ], alpha = 0.7) +
+  geom_line(lty = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~ raw.or.dig) +
+  theme_bw() +
+  labs(x = 'Application period', y = 'Emission (% of TAN)',
+       shape = '', colour = '') +
+  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)),
+                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+  ylim(0, 62) +
+  theme(legend.position = 'top') +
+  scale_color_manual(values = c("#1B9E77",  "#D95F02","#E7298A","#7570B3"))
+ggsave('../plots/emis_pred2_JP2.png', height = 4, width = 7)
+
+ggplot(dd, aes(app.timing.int, EFp, colour = man.source)) +
+  geom_line(data = ul, alpha = 0.7) +
+  geom_line(data = ulc, alpha = 0.7) +
+  geom_line(lty = 1) +
+  geom_point(size = 3) +
+  facet_wrap(~ raw.or.dig) +
+  theme_bw() +
+  labs(x = 'Application period', y = 'Emission (% of TAN)',
+       shape = '', colour = '') +
+  scale_shape_manual(values = c(19, 24, 6, 20, 1, 3, 7, 2, 4, 8)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)),
+                     labels= unique(dat$app.timing), limits = c(0.8, 6.1)) +
+  ylim(0, 62) +
+  theme(legend.position = 'top') +
+  scale_color_manual(values = c("#1B9E77", "#D95F02","#E7298A", "#66A61E", "#7570B3"))
+ ggsave('../plots/emis_pred2_JP3.png', height = 4, width = 7)
