eb7249fa0dfbef50c8528cadf737d83914efae6f
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-10-25 09:04:16 +0200

    working on plots for presentation

--- lit_raw_vs_dig/scripts/plot.R
+++ lit_raw_vs_dig/scripts/plot.R
@@ -121,37 +121,3 @@ ggplot(dw, aes((DM.dig - DM.ref), (pH.dig - pH.ref), colour = interaction(source
   theme(legend.position = 'none')
 ggsave2x('../plots/d_DM_pH', height = 4, width = 4)
 
-
-# JP. Plots for presentation 231031 at 'themeday for biogas' hosted by SEGES
-
-library(plyr)
-dw1 <- dw
-dw1$slurry.major.ref <- mapvalues(dw1$slurry.major.ref, from = 'Cattle', to = 'Kvæggylle')
-dw1$slurry.major.ref <- mapvalues(dw1$slurry.major.ref, from = 'Pig', to = 'Svinegylle')
-
-ggplot(dw1, aes(emis.perc.ref, emis.perc.dig, colour = slurry.major.ref)) +
-  geom_abline(intercept = 0, slope = 0, linetype = 2) + 
-  geom_point(shape = 1, size = 6) +
-  geom_text(aes(label = source.ID), size = 3, show.legend = FALSE) +
-  geom_abline(intercept = 0, slope = 1, linetype = 2) +
-  scale_color_brewer(palette = "Set1") +
-  theme_bw() +
-  theme(legend.position = 'top') + 
-  labs(x = 'Ubehandlet EF (% of TAN)', y = 'Afgasset EF (% of TAN)', shape = '', colour = '', lty = '') +
-  xlim(8, 62) + ylim(8, 62)
-ggsave2x('../plots/dig_effect_JP231024', height = 4.3, width = 4.8)
-
-dd1 <- dd
-dd1$relDiff.frac.nm <- mapvalues(dd1$relDiff.frac.nm, from = 'Raw', to = 'Ubehandlet')
-dd1$relDiff.frac.nm <- mapvalues(dd1$relDiff.frac.nm, from = 'Digestate', to = 'Afgasset')
-
-ggplot(dd1, aes(DM, pH, shape = relDiff.frac.nm, colour = interaction(source, relDiff.set))) +
-  geom_smooth(aes(group = interaction(source, relDiff.set)), method = lm, se = FALSE, linewidth = 0.5) +
-  scale_shape_manual(values = c(1, 17)) +
-  geom_point(size = 3) +
-  labs(x = 'TS (%)', y = 'pH', shape = '') +
-  theme_bw() +
-  theme(legend.position = 'top') +
-  guides(colour = 'none')
-ggsave2x('../plots/DM_pH_effects_JP231024', height = 4.3, width = 4.8)
-
